3b3f3da40d5b62ed82de17a77e1be53c31455797
KagariSoft-Dev <dev@kagarisoft.com>
2022-11-11 12:21:51 -0300

    update server.js

--- 06.Servidores/server.js
+++ 06.Servidores/server.js
@@ -7,17 +7,20 @@ app.get('/', (req, res) => {
       res.send('Hello World!')
 })
 
+/* This is a route that will return all products from the database. */
 app.get('/api/products', async (req, res) => {
       const all = await Product.getAll()
       res.json(all)
 })
 
+/* This is a route that will return a random product from the database. */
 app.get('/api/products/random', async (req, res) => {
       const random = await Product.getRandom()
       res.json(random)
 })
 
 
+/* This is a catch-all route. It will catch any request that does not match any of the other routes. */
 app.get('*', (req, res) => {
       res.status(404).json({
             error: {
