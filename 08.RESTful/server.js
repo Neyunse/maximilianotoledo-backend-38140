ed5b1f2f425d721b7ed7573392dc6cc82f5a312e
KagariSoft-Dev <dev@kagarisoft.com>
2022-11-28 13:06:12 -0300

    update .gitignore and server.js

--- 08.RESTful/server.js
+++ 08.RESTful/server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 8080;
 
 let endpoints = require("./api");
 
-app.use(express.static('html'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 app.use(cors("*"));
 
